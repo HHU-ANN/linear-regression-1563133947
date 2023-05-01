98b8283a374711fe274027d4a5e1af808cc285a0
1563133947 <101780373+1563133947@users.noreply.github.com>
2023-05-02 01:37:00 +0800

    Update Linear_Regression.py

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -10,22 +10,26 @@ except ImportError as e:
 
 def ridge(data):
     X,Y=read_data()
-    w=np.matmul(np.linalg.inv(np.matmul(X.T,X)),np.matmul(X.T,Y))
-    return w *data
+    lamuda=0.01
+    t1=np.linalg.inv((np.matmul(X.T,X)+lamuda*np.eye(X.shape[1])))
+    w_r=np.matmul(t1,np.matmul(X.T,Y))
+    return sum(w_r*data)
     
 def lasso(data):
+    X,Y=read_data() # 404*1
+    alpha=1e-15
+    step=1e-15
     item=10000
-    min=0.0001
-    alpha=1e-5
-    X,Y=read_data()
-    m,n = X.shape
-    w2 = np.zeros(n)
+    tol=0.0001
+    m,n= X.shape
+    w = np.zeros(n)
     for i in range(item):
-        grad=np.matmul(X.T,np.matmul(X,w2)-Y)+alpha*np.sign(w2)
-        w2=w2-alpha*grad
-        if np.linalg.norm(grad)<min:
+        Y_hat=np.dot(X,w)
+        grad=np.dot(X.T,Y_hat-Y)/m+alpha*np.sign(w)
+        w=w-step*grad
+        if np.linalg.norm(grad)<tol:
             break
-    return w2 *data
+    return sum(w*data)
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
